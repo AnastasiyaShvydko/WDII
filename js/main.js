a6a11bc095b9cc7793183a1a828a9c0245405273
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2022-11-13 00:21:59 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ url: 'week3/index.html'}, {label: 'Week04',
 url: 'week4/index.html'}, {label: 'Week05',
 url: 'week5/index.html'} , {label: 'Week07',
 url: 'week7/index.html'}, {label: 'Week08',
-url: 'week8/index.html'}]
+url: 'week8/index.html'},{label: 'Week09',
+url: 'week9/index.html'}]
 
 function loadLinks(){
 for (var i = 0; i < myArray.length; i++){
